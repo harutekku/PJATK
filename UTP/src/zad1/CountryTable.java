c6a8b051ab37aa9230a9cf46c2d77e29a10a9f09
kubbit98 <Mordek1998>
2019-10-04 18:22:15 +0200

    utp

---
+++ UTP/src/zad1/CountryTable.java
@@ -0,0 +1,14 @@
+package zad1;
+
+import javax.swing.*;
+
+public class CountryTable {
+    private String[] columnNames;
+    public CountryTable(String countriesFileName) {
+
+    }
+
+    public JTable create() {
+        return new JTable();
+    }
+}
