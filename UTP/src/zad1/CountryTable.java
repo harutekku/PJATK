5cc665b89ba1e1bfede983c305c151c28d633edd
Kubbit98 <kuba98pawlowicz@gmail.com>
2019-10-06 23:56:06 +0200

    dziala super

--- UTP/src/zad1/CountryTable.java
+++ UTP/src/zad1/CountryTable.java
@@ -1,21 +1,25 @@
 package zad1;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class CountryTable {
-    private ArrayList<String[]> countries;
+    private ArrayList<Object[]> countries;
     private String[] columnNames;
-    private String[][] data;
+    private Object[][] data;
+    private JTable jTable;
+
     BufferedReader br;
     StringTokenizer st;
 
     public CountryTable(String countriesFileName) {
         try {
             br = new BufferedReader(new FileReader("data/countries.txt"));
-            countries=new ArrayList<>();
+            countries = new ArrayList<>();
             readColumnNames();
             readCountries();
         } catch (IOException e) {
@@ -24,19 +28,32 @@ public class CountryTable {
     }
 
     public JTable create() {
-        System.out.println(columnNames[0]);
-        System.out.println(columnNames[1]);
-        System.out.println(columnNames[2]);
-        return new JTable(data,columnNames);
+        return new JTable(data, columnNames).getColumn("Population").setCellRenderer(
+                new DefaultTableCellRenderer() {
+                    @Override
+                    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                        DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                        if(column==3) {
+                            if ((int) value > 20000) {
+                                renderer.setForeground(Color.red);
+                            }
+                        }else {
+                            Color originalColor=Color.BLACK;
+                            renderer.setForeground(originalColor); // Retore original color
+                        }
+                        return renderer;
+                    }
+                }
+        );
     }
 
     private void readColumnNames() {
         try {
             st = new StringTokenizer(br.readLine(), "\t");
             columnNames = new String[st.countTokens()];
-            int i=0;
+            int i = 0;
             while (st.hasMoreTokens()) {
-                columnNames[i]=st.nextToken();
+                columnNames[i] = st.nextToken();
                 i++;
             }
         } catch (IOException e) {
@@ -46,40 +63,45 @@ public class CountryTable {
 
     private void readCountries() {
         try {
-            String line=br.readLine();
-            do{
+            String line = br.readLine();
+            do {
                 st = new StringTokenizer(line, "\t");
                 readCountryData();
-                line=br.readLine();
-            }while (line!=null);
-            data=new String[countries.size()][columnNames.length];
-            for(int i=0;i<countries.size();i++){
-                data[i]=countries.get(i);
+                line = br.readLine();
+            } while (line != null);
+            data = new Object[countries.size()][columnNames.length];
+            for (int i = 0; i < countries.size(); i++) {
+                data[i] = countries.get(i);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
     private void readCountryData() {
-        String[] ctr=new String[columnNames.length];
-        int i=0;
-        while(st.hasMoreTokens()){
-            ctr[i++]=st.nextToken();
+        Object[] ctr = new Object[columnNames.length];
+        ctr[0] = st.nextToken();
+        ctr[1] = st.nextToken();
+        ctr[2] = st.nextToken();
+        int population = Integer.valueOf(st.nextToken());
+        population /= 1000;
+        if (population == 0) population = 1;
+        ctr[3] = population;
+        if (population > 20000) {
+            DefaultTableCellRenderer cell = new DefaultTableCellRenderer();
 
         }
-        if(i==2){
-            ctr[2]=ctr[1];
-            ctr[1]="";
-        }
-        if(ctr[2].length()>=4){
-            ctr[2]=ctr[2].substring(0,ctr[2].length()-3);
-        }
-        else{
-            ctr[2]="<1";
-        }
+        //Integer population=Integer.valueOf();
+        /*if((Integer)ctr[3]==0){
+            ctr[3]=Integer.valueOf("1");
+        }*/
 
 
+        /*System.out.println(ctr[0]);
+        System.out.println(ctr[1]);
+        System.out.println(ctr[2]);
+        System.out.println(ctr[3]);*/
         countries.add(ctr);
     }
 
