74a298613b7bd3de11e6a5a36a6a93d789d7bd45
Kubbit98 <kuba98pawlowicz@gmail.com>
2019-10-08 23:44:06 +0200

    dzialajace utp

--- UTP/src/zad1/CountryTable.java
+++ UTP/src/zad1/CountryTable.java
@@ -1,82 +1,51 @@
 package zad1;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class CountryTable {
-    private ArrayList<Object[]> countries;
+class CountryTable {
+    private List<Object[]> countries;
     private String[] columnNames;
     private Object[][] data;
     private JTable jTable;
+    private BufferedReader br;
+    private StringTokenizer st;
 
-    BufferedReader br;
-    StringTokenizer st;
+    CountryTable(String countriesFileName) throws IOException {
+        br = new BufferedReader(new FileReader(countriesFileName));
+        countries = new ArrayList<>();
+        readColumnNames();
+        readCountries();
+    }
 
-    public CountryTable(String countriesFileName) {
-        try {
-            br = new BufferedReader(new FileReader("data/countries.txt"));
-            countries = new ArrayList<>();
-            readColumnNames();
-            readCountries();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void readColumnNames() throws IOException {
+        st = new StringTokenizer(br.readLine(), "\t");
+        columnNames = new String[st.countTokens()];
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            columnNames[i] = st.nextToken();
+            i++;
         }
     }
 
-    public JTable create() {
-        return new JTable(data, columnNames).getColumn("Population").setCellRenderer(
-                new DefaultTableCellRenderer() {
-                    @Override
-                    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-                        DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                        if(column==3) {
-                            if ((int) value > 20000) {
-                                renderer.setForeground(Color.red);
-                            }
-                        }else {
-                            Color originalColor=Color.BLACK;
-                            renderer.setForeground(originalColor); // Retore original color
-                        }
-                        return renderer;
-                    }
-                }
-        );
-    }
-
-    private void readColumnNames() {
-        try {
-            st = new StringTokenizer(br.readLine(), "\t");
-            columnNames = new String[st.countTokens()];
-            int i = 0;
-            while (st.hasMoreTokens()) {
-                columnNames[i] = st.nextToken();
-                i++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void readCountries() throws IOException {
+        String line = br.readLine();
+        do {
+            st = new StringTokenizer(line, "\t");
+            readCountryData();
+            line = br.readLine();
+        } while (line != null);
+        data = new Object[countries.size()][columnNames.length];
+        for (int i = 0; i < countries.size(); i++) {
+            data[i] = countries.get(i);
         }
-    }
-
-    private void readCountries() {
-        try {
-            String line = br.readLine();
-            do {
-                st = new StringTokenizer(line, "\t");
-                readCountryData();
-                line = br.readLine();
-            } while (line != null);
-            data = new Object[countries.size()][columnNames.length];
-            for (int i = 0; i < countries.size(); i++) {
-                data[i] = countries.get(i);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     private void readCountryData() {
@@ -88,25 +57,47 @@ public class CountryTable {
         population /= 1000;
         if (population == 0) population = 1;
         ctr[3] = population;
-        if (population > 20000) {
-            DefaultTableCellRenderer cell = new DefaultTableCellRenderer();
-
-        }
-        //Integer population=Integer.valueOf();
-        /*if((Integer)ctr[3]==0){
-            ctr[3]=Integer.valueOf("1");
-        }*/
-
-
-        /*System.out.println(ctr[0]);
-        System.out.println(ctr[1]);
-        System.out.println(ctr[2]);
-        System.out.println(ctr[3]);*/
         countries.add(ctr);
     }
 
-    public static void main(String[] args) {
-        new CountryTable("data/countries.txt");
-    }
+    JTable create() {
+        jTable = new JTable(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        jTable.setRowHeight(35);
+        jTable.getColumn("Flag").setMaxWidth(50);
+        jTable.getColumn("Flag").setCellRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                File file = new File("data/flag_icons/" + value + ".png");
+                BufferedImage image;
 
+                try {
+                    image = ImageIO.read(file);
+                } catch (IOException e) {
+                    return new JLabel();
+                }
+                return new JLabel(new ImageIcon(image.getScaledInstance(40, 28, 1)));
+            }
+        });
+        jTable.getColumn("Population(k)").setCellRenderer(
+                new DefaultTableCellRenderer() {
+                    @Override
+                    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                        DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                        renderer.setHorizontalAlignment(JLabel.RIGHT);
+                        if ((int) value > 20000) {
+                            renderer.setForeground(Color.red);
+                        } else {
+                            renderer.setForeground(Color.BLACK);
+                        }
+                        return renderer;
+                    }
+                }
+        );
+        return jTable;
+    }
 }
