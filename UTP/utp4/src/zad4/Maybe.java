edd63d1ec1f52c64ea6b4d17efbaddad919013e9
Kubbit98 <kuba98pawlowicz@gmail.com>
2019-11-06 23:02:46 +0100

    utp4 zad 4 dziala w koncu omg

--- UTP/utp4/src/zad4/Maybe.java
+++ UTP/utp4/src/zad4/Maybe.java
@@ -6,38 +6,42 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Maybe<T>{
-    T t;
+	T t;
 
-    public Maybe(T t){
-        this.t=t;
-    }
+	public Maybe(T t){
+		this.t=t;
+	}
 
-    public static <T> Maybe<T> of(T t){
-        return new Maybe<>(t);
-    }
+	public static <T> Maybe<T> of(T t){
+		return new Maybe<T>(t);
+	}
 
-    public void ifPresent(Consumer consumer){
-        if(this.t!=null) consumer.accept(t);
-    }
+	public void ifPresent(Consumer consumer){
+		if(this.t!=null) consumer.accept(t);
+	}
 
-    public Maybe<T> map(Function func){
-        return this.t!=null?new Maybe(func.apply(this.t)):new Maybe(null);
-    }
+	public <S> Maybe <S> map(Function<T,S> func){
+		return this.t!=null?new Maybe<S>(func.apply(this.t)):new Maybe<S>(null);
+	}
 
-    public T get(){
-        if(this.t!=null) return this.t;
-        else throw new NoSuchElementException();
-    }
+	public T get(){
+		if(this.t!=null) return this.t;
+		else throw new NoSuchElementException(" maybe is empty");
+	}
 
-    public boolean isPresent(){
-        return this.t!=null;
-    }
+	public boolean isPresent(){
+		return this.t!=null;
+	}
 
-    public T orElse(T defVal){
-        return isPresent()?t:defVal;
-    }
+	public T orElse(T defVal){
+		return isPresent()?t:defVal;
+	}
 
-    public<T> Maybe<T> filter(Predicate<T> pred){
-        return (pred.test((T)t) && isPresent())?new Maybe(t):new Maybe(null);
-    }
+	public Maybe<T> filter(Predicate<T> pred){
+		return (pred.test(t)&&isPresent())?this:new Maybe(null);
+	}
+
+	public String toString() {
+		return isPresent() ? "Maybe has value " + t : "Maybe is empty";
+	}
 }
---
+++ UTP/utp4/src/zad5/XList.java
@@ -0,0 +1,6 @@
+package zad5;
+
+public class XList<T>{
+	public XList(T... array){
+	}
+}
