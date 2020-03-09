17298f88e3759a7cb3b7cf8a04e8aa1050d4f867
Kubbit98 <kuba98pawlowicz@gmail.com>
2020-03-10 00:10:50 +0100

    PSM poprawa pd1, dodaje pri pd1

--- PSM/zad1/Program.cs
+++ PSM/zad1/Program.cs
@@ -6,37 +6,49 @@ namespace zad1
     {
         static void Main(string[] args)
         {
-            int n = 20;//dlugosc rozwiniecia
-            sin(0, n);
-            sin(90, n);
-            sin(180, n);
-            sin(270, n);
-            sin(360, n);
-            sin(360+90, n);
-            sin(15, n);
-            sin(-15, n);
-
+            double pi = Math.PI;
+            sin(0,true);
+            sin(90, true);
+            sin(180, true);
+            sin(270, true);
+            sin(360, true);
+            sin(360+90, true);
+            sin(45, true);
+            sin(-45, true);
             System.Console.WriteLine();
+            sin(0, false);
+            sin(pi/2, false);
+            sin(pi, false);
+            sin(pi*3/2, false);
+            sin(pi*2, false);
+            sin(pi/4, false);
+            sin(pi/-4, false);
             System.Console.WriteLine();
             System.Console.WriteLine();
             Random rd = new Random();
-            for(int i = 0; i < 50; i++)
+            for(int i = 0; i < 10; i++)
             {
-                sin(rd.Next(-720, 720), rd.Next(0,20));
+                sin(rd.Next(-720, 720), rd.Next(0,10)>5);
             }
         }
-        static double sin(double deg, int n)
+        static double sin(double deg, bool czyStopnie)
         {
+            if (!czyStopnie)
+            {
+                deg = deg * 180 / Math.PI;
+            }
             deg = deg % 360;
             double x;
             bool positive = true;
             if (deg <= 90)
             {
                 x = deg;
-            }else if (deg <= 180)
+            }
+            else if (deg <= 180)
             {
                 x = 180 - deg;
-            }else if (deg <= 270)
+            }
+            else if (deg <= 270)
             {
                 x = deg - 180;
                 positive = !positive;
@@ -51,24 +63,23 @@ namespace zad1
                 Console.Error.WriteLine("Error");
                 return -1;
             }
+            bool positiveForMath = positive;
             double rad = ((x * Math.PI) / 180);
-            
             double result = 0;
             int hop = 1;
-            while (hop<=n)
+            double factorial = 1;
+            Console.WriteLine("Sinus z " + (czyStopnie?deg+" stopni":rad+" radianów"));
+            for(int i=1;i<=10;i++)
             {
-                if (positive) result += Math.Pow(rad, hop) / factorial(hop);
-                else result -= Math.Pow(rad, hop) / factorial(hop);
-                hop += 2;
+                if (positive) result += Math.Pow(rad, hop) / factorial;
+                else result -= Math.Pow(rad, hop) / factorial;
+                factorial *= ++hop;
+                factorial *= ++hop;
                 positive = !positive;
+                Console.WriteLine("o kroku " + i + "\twynosi " + result + "\tz biblioteki " + Math.Sin(positiveForMath ? rad : rad * -1) + 
+                    "\t roznica " + Math.Abs(result - Math.Sin(positiveForMath ? rad : rad * -1)));
             }
-            Console.WriteLine("Sinus z " + deg + " o rozwinięciu " + n + " wynosi " + result);
-            return result;
-        }
-        static double factorial(double fac)
-        {
-            double result = 1;
-            while (fac > 1) result *= fac--;
+            Console.WriteLine();
             return result;
         }
     }
