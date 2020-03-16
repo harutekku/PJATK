4ad647c37326cb0e64126af5db66ecda98042763
kubbit98 <kuba98pawlowicz@gmail.com>
2020-03-16 21:05:10 +0100

    cw3, faza pisanka

---
+++ APBD/cw3/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public String getStudent(String orderby)
+        {
+            return $"Kocham Magde sortowanie={orderby}";
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else
+            {
+                return NotFound("nie znaleziono");
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+    }
+}
---
+++ APBD/cw3/Services/MockDbService.cs
@@ -0,0 +1,19 @@
+﻿using cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Services
+{
+    public class MockDbService
+    {
+        private static IEnumerable<Student> _students = new List<Student>
+        {
+            new Student{IdStudent=1,FirstName="Jan",LastName="Man",IndexNumber="s1000" },
+            new Student{IdStudent=2,FirstName="Dzban",LastName="Cham",IndexNumber="s1001" },
+            new Student{IdStudent=3,FirstName="Kan",LastName="Ram",IndexNumber="s1002" },
+            new Student{IdStudent=4,FirstName="Lan",LastName="Pam",IndexNumber="s1003" }
+        };
+    }
+}
