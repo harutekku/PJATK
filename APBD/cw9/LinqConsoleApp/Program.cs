2f707b0fc204fa632cead3e93b8110b27b5a2b55
kubbit98 <kuba98pawlowicz@gmail.com>
2020-05-06 14:04:51 +0200

    APBD cw 9

---
+++ APBD/cw9/LinqConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Przyklad1();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad2();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad3();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad4();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad5();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad6();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad7();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad8();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad9();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad10Button_Click();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad11();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad12();
+        }
+    }
+}
