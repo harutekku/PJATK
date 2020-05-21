01501cf0e2dcdc9e208231dc5a0e8fe0cda842bc
kubbit98 <kuba98pawlowicz@gmail.com>
2020-05-21 13:52:28 +0200

    apbd cw11 add some data

---
+++ APBD/cw11/Migrations/20200521090311_AddedSomeData.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cw11.Migrations
+{
+    public partial class AddedSomeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "PrescriptionMedicament_Medicament",
+                table: "Prescription_Medicament");
+
+            migrationBuilder.DropForeignKey(
+                name: "PrescriptionMedicament_Prescription",
+                table: "Prescription_Medicament");
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "kuba@ziemniak.com", "Kuba", "Ziemniak" },
+                    { 2, "rafal@balut.com", "Rafał", "Balut" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicaments",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Antivirus", "Paracetamol", "Liquid" },
+                    { 2, "Antivirus", "Amol", "Liquid" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "BirthDate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1990, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Julian", "Mikołajczyk" },
+                    { 2, new DateTime(1995, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Julian", "Mikołajczyk" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateTime(2020, 5, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 8, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 2, new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdPrescription", "IdMedicament", "Details", "Dose" },
+                values: new object[] { 1, 1, "2 razy dziennie", 10 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdPrescription", "IdMedicament", "Details", "Dose" },
+                values: new object[] { 2, 2, "4 razy dziennie", 3 });
+
+            migrationBuilder.AddForeignKey(
+                name: "PrescriptionMedicament_Medicament",
+                table: "Prescription_Medicament",
+                column: "IdMedicament",
+                principalTable: "Medicaments",
+                principalColumn: "IdMedicament",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "PrescriptionMedicament_Prescription",
+                table: "Prescription_Medicament",
+                column: "IdPrescription",
+                principalTable: "Prescriptions",
+                principalColumn: "IdPrescription",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "PrescriptionMedicament_Medicament",
+                table: "Prescription_Medicament");
+
+            migrationBuilder.DropForeignKey(
+                name: "PrescriptionMedicament_Prescription",
+                table: "Prescription_Medicament");
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdPrescription", "IdMedicament" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdPrescription", "IdMedicament" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 2);
+
+            migrationBuilder.AddForeignKey(
+                name: "PrescriptionMedicament_Medicament",
+                table: "Prescription_Medicament",
+                column: "IdMedicament",
+                principalTable: "Medicaments",
+                principalColumn: "IdMedicament",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "PrescriptionMedicament_Prescription",
+                table: "Prescription_Medicament",
+                column: "IdPrescription",
+                principalTable: "Prescriptions",
+                principalColumn: "IdPrescription",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- APBD/cw11/Models/ClinicDbContext.cs
+++ APBD/cw11/Models/ClinicDbContext.cs
@@ -22,15 +22,105 @@ namespace cw11.Models
             modelBuilder.Entity<Prescription_Medicament>().HasOne(d => d.Prescription)
                 .WithMany(e => e.Prescription_Medicament)
                 .HasForeignKey(d => d.IdPrescription)
-                .OnDelete(DeleteBehavior.ClientSetNull)
+                .OnDelete(DeleteBehavior.Cascade)
                 .HasConstraintName("PrescriptionMedicament_Prescription");
 
             modelBuilder.Entity<Prescription_Medicament>().HasOne(d => d.Medicament)
                 .WithMany(e => e.Prescription_Medicament)
                 .HasForeignKey(d => d.IdMedicament)
-                .OnDelete(DeleteBehavior.ClientSetNull)
+                .OnDelete(DeleteBehavior.Cascade)
                 .HasConstraintName("PrescriptionMedicament_Medicament");
-            //
+
+
+            var doctors = new List<Doctor>();
+            doctors.Add(new Doctor
+            {
+                Id = 1,
+                FirstName = "Kuba",
+                LastName = "Ziemniak",
+                Email = "kuba@ziemniak.com"
+            });
+            doctors.Add(new Doctor
+            {
+                Id = 2,
+                FirstName = "Rafał",
+                LastName = "Balut",
+                Email = "rafal@balut.com"
+            });
+
+            var patients = new List<Patient>();
+            patients.Add(new Patient
+            {
+                IdPatient = 1,
+                FirstName = "Julian",
+                LastName = "Mikołajczyk",
+                BirthDate = DateTime.Parse("20-05-1990")
+            });
+            patients.Add(new Patient
+            {
+                IdPatient = 2,
+                FirstName = "Julian",
+                LastName = "Mikołajczyk",
+                BirthDate = DateTime.Parse("02-01-1995")
+            });
+            var medicaments = new List<Medicament>();
+            medicaments.Add(new Medicament
+            {
+                IdMedicament = 1,
+                Name = "Paracetamol",
+                Description = "Antivirus",
+                Type = "Liquid"
+            });
+            medicaments.Add(new Medicament
+            {
+                IdMedicament = 2,
+                Name = "Amol",
+                Description = "Antivirus",
+                Type = "Liquid"
+            });
+            var prescriptions = new List<Prescription>();
+            prescriptions.Add(new Prescription
+            {
+                IdPrescription = 1,
+                Date = DateTime.Parse("14-05-2020"),
+                DueDate = DateTime.Parse("14-08-2020"),
+                IdDoctor = 1,
+                IdPatient = 1
+            });
+            prescriptions.Add(new Prescription
+            {
+                IdPrescription = 2,
+                Date = DateTime.Parse("10-01-2020"),
+                DueDate = DateTime.Parse("10-06-2020"),
+                IdDoctor = 2,
+                IdPatient = 2
+            });
+            
+            var prescriptionMedicaments = new List<Prescription_Medicament>();
+            prescriptionMedicaments.Add(new Prescription_Medicament
+            {
+                Dose = 10,
+                Details = "2 razy dziennie",
+                IdMedicament = 1,
+                IdPrescription = prescriptions[0].IdDoctor
+            });
+            prescriptionMedicaments.Add(new Prescription_Medicament
+            {
+                Dose = 3,
+                Details = "4 razy dziennie",
+                IdMedicament = 2,
+                IdPrescription = prescriptions[1].IdDoctor
+            });
+
+            modelBuilder.Entity<Doctor>().HasData(doctors);
+
+            modelBuilder.Entity<Patient>().HasData(patients);
+
+            modelBuilder.Entity<Medicament>().HasData(medicaments);
+
+            modelBuilder.Entity<Prescription>().HasData(prescriptions);
+
+            modelBuilder.Entity<Prescription_Medicament>().HasData(prescriptionMedicaments);
         }
 
     }
